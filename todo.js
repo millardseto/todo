b4bb85622122433d1f052a2048b522f1122b50a4
Millard Seto <millardseto@mac.com>
2017-06-13 20:31:55 -0700

    show task list

--- todo.js
+++ todo.js
@@ -1,14 +1,37 @@
 window.onload = function(){
   function addTask(){
+    // block the default behavior
     event.preventDefault();
+
+    // get form values
     var form = document.forms[0];
     //var task = document.getElementById('task').value;
     var task = form["task"].value;
     var assignTo = form["assignTo"].value;
     var level = form["level"].value;
 
+    // add new item to tasks
     tasks.push({task:task, assignTo:assignTo, level:level});
 
+    // update list
+    showList();
+  }
+
+  function showList(){
+    // clear existing content
+    var ul = document.querySelector('ul');
+    ul.innerText = "";
+
+    // go through each task
+    for(var i=0; i<tasks.length; i++) {
+      // create list item and text
+      var li = document.createElement("li");
+      li.innerText = tasks[i].task;
+
+
+      // add the list item to the list
+      ul.appendChild(li);
+    }
   }
 
   // global container for tasks
